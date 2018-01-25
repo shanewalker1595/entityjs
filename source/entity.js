1366be2e2e09aa2edf8a561ce3b46f0a36d0b723
Shane Walker <swalker1595@gmail.com>
2018-01-24 19:12:35 -0500

    Created the base layout of the db and table objects.

--- source/entity.js
+++ source/entity.js
@@ -1,29 +1,55 @@
 ;(function () {
     var db = function () {
-        this.CreateTable =  function (tableName, databaseName) {
-            if (typeof databaseName === "undefined") databaseName = this.databaseName;
-            if (typeof databaseName === "undefined") throw "You must provide a database name when creating a table.";
-            var request = window.indexedDB.open(databaseName);
-        }
-        return this;
-    }
-    db.prototype = IDBDatabase;
+        
+    },
+    table = function () {
 
-    window.Entityjs = {
-        CreateDb: function (databaseName, version, success, fail, upgrade) {
+    };
+    db.prototype.createTable = function (tableName) {
+
+    };
+    db.prototype.deleteTable = function (tableName) {
+
+    };
+    table.prototype.getById = function (id) {
+
+    };
+    table.prototype.select = function (selectorFn) {
+        
+    };
+    table.prototype.removeById = function (id) {
+
+    };
+    table.prototype.remove = function (entity) {
+
+    };
+    table.prototype.update = function (entity) {
+
+    };
+    table.prototype.where = function (whereFn) {
+        
+    }
+
+    // Creates an EntityJs db from and IIndexedDb object.
+    function _createDb(indexDb) {
+        var newDb = new db();
+        newDb.originalDb = indexDb;
+        return newDb;
+    }
+
+    window.EntityJs = {
+        CreateDb: function (databaseName, version, success, fail, update) {
             var request = window.indexedDB.open(databaseName, version);
             request.onsuccess = function (e) {
-                Object.setPrototypeOf(e.target.result, new db());
-                success(e.target.result);
+                success(_createDb(request.result));
             }
             request.onerror = function (e) {
-                fail(e.target.result);
+                fail(e);
             }
             request.onupgradeneeded = function (e) {
-                Object.setPrototypeOf(e.target.result, x);
-                e.target.result.prototype = db;
-                upgrade(e.target.result);
-                success(e.target.result);
+                var db = _createDb(request.result);
+                upgrade(db);
+                success(db);
             }
         }
     }
